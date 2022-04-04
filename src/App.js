f80f2265cad93d26520c41e766abfcd141c8445c
unknown <tamiloreolaniyi@gmail.com>
2022-04-04 21:54:26 +0100

    moved files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
+import HomePage from './pages/homepage/homepage.component';
+import { Switch, Route } from 'react-router-dom';
+import ShopPage from './pages/Shop/shop.component';
+import Header from './components/header/Header.jsx'
+import SignInAndSignUpPage from './pages/Sign-in-and-sign-up/sign-in-and-sign-up.jsx'
+
 
 function App() {
   return (
-    <div className="App">
-      
+    <div >
+      <Header/>
+      <Switch>
+      <Route exact path='/' component={HomePage}></Route>
+      <Route path='/shop' component={ShopPage}></Route>
+      <Route path='/signin' component={SignInAndSignUpPage}></Route>
+      </Switch>
+       
     </div>
   );
 }
