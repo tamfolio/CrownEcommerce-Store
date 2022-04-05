ac13f27f261cb81fbca5140f050d0ec526fe6661
unknown <tamiloreolaniyi@gmail.com>
2022-04-05 10:26:56 +0100

    sign up component

--- src/components/sign-up/sign-up.jsx
+++ src/components/sign-up/sign-up.jsx
@@ -19,16 +19,43 @@ class SignUp extends React.Component {
     }
 
     render() {
+        const { displayName, email,password,confirmPassword} = this.state
         return (
             <div className="sign-up">
                 <h2 className="title">I do not have an account</h2>
                 <span>Sign up with your email and password</span>
                 <form className="sign-up-form" onSubmit={this.handleSubmit}>
-                    <FormInput>
-                        
-                    </FormInput>
+                    <FormInput type='text'
+                    name='displayName'
+                    value={displayName}
+                    onChange={this.handleChange}
+                    label='Display Name'
+                    />
+                    <FormInput type='email'
+                    name='email'
+                    value={email}
+                    onChange={this.handleChange}
+                    label='Email'
+                    />
+                    <FormInput type='password'
+                    name='password'
+                    value={password}
+                    onChange={this.handleChange}
+                    label='Password'
+                    required
+                    />
+                    <FormInput type='password'
+                    name='confirmPassword'
+                    value={displayName}
+                    onChange={this.handleChange}
+                    label='Confirm Password'
+                    required
+                    />
+                    <CustomButton type='submit'>SIGN UP</CustomButton>
                 </form>
             </div>
         )
     }
 }
+
+export default SignUp
