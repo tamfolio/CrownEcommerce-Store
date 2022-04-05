c0121d121fe3ff8c0f0ec8224d45c53cf358b825
unknown <tamiloreolaniyi@gmail.com>
2022-04-05 10:43:58 +0100

    sign up form and styles

--- src/components/sign-up/sign-up.jsx
+++ src/components/sign-up/sign-up.jsx
@@ -18,6 +18,40 @@ class SignUp extends React.Component {
         }
     }
 
+    handleSubmit = async event => {
+        event.preventDefault();
+        const { displayName, email,password,confirmPassword} = this.state
+
+        if(password !== confirmPassword){
+            alert("passwords don't match")
+            return;
+        }
+
+        try {
+            const { user } = await auth.createUserWithEmailAndPassword(email,password);
+        
+            await    createUserProfileDocument(user, {displayName})
+
+            this.setState({
+                displayName: '',
+                email:'',
+                password:'',
+                confirmPassword: ''
+            })
+
+        } catch (error) {
+            console.error(error)
+        }
+
+
+    };
+
+    handleChange = event => {
+        const { name, value } = event.target;
+
+        this.setState({ [name]: value });
+    }
+
     render() {
         const { displayName, email,password,confirmPassword} = this.state
         return (
@@ -46,7 +80,7 @@ class SignUp extends React.Component {
                     />
                     <FormInput type='password'
                     name='confirmPassword'
-                    value={displayName}
+                    value={confirmPassword}
                     onChange={this.handleChange}
                     label='Confirm Password'
                     required
