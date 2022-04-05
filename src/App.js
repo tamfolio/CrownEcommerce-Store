148aafc570b2413ad4d8ac0ffb88fb93570de429
unknown <tamiloreolaniyi@gmail.com>
2022-04-05 04:36:39 +0100

    auth

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
 import './App.css';
+import React from 'react'
 import HomePage from './pages/homepage/homepage.component';
 import { Switch, Route } from 'react-router-dom';
 import ShopPage from './pages/Shop/shop.component';
 import Header from './components/header/Header.jsx'
 import SignInAndSignUpPage from './pages/Sign-in-and-sign-up/sign-in-and-sign-up.jsx'
+import { auth } from './firebase/firebase.utils'
 
 
-function App() {
+class  App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      currentUser: null
+    }
+  }
+
+  componentDidMount(){
+    auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user});
+
+      console.log(user);
+    })
+  }
+
+  componentWillUnmount(){
+    this.unsubscribeFromAuth();
+  }
+
+
+  render() {
   return (
-    <div >
+    <div>
       <Header/>
       <Switch>
       <Route exact path='/' component={HomePage}></Route>
@@ -18,6 +42,7 @@ function App() {
        
     </div>
   );
+  }
 }
 
 export default App;
