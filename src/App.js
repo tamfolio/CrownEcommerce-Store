d80b0f30b5465727f8071dc1e0ef25955aea94d7
unknown <tamiloreolaniyi@gmail.com>
2022-04-05 09:52:58 +0100

    firebase firestore

--- src/App.js
+++ src/App.js
@@ -24,9 +24,18 @@ class  App extends React.Component {
       if(userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
         userRef.onSnapshot(snapShot => {
-          
+         this.setState({
+           currentUser: {
+             id: snapShot.id,
+             ...snapShot.data()
+           }
+         }, () => {
+          console.log(this.state);
+         })
         })
+        
       }
+      this.setState({ currentUser: userAuth});
     })
   }
 
