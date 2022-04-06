3f368a8e20f3b0187091b1f377efc9414dd73f51
unknown <tamiloreolaniyi@gmail.com>
2022-04-06 08:47:55 +0100

    redux basics

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux'
 
 ReactDOM.render(
+  <Provider>
   <BrowserRouter>
     <App />
-  </BrowserRouter>,
+  </BrowserRouter>
+  </Provider>,
   document.getElementById('root') 
 );
 
