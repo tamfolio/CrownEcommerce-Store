483d49e4a3dbe6e1e73610db4643cdedd9bdf131
unknown <tamiloreolaniyi@gmail.com>
2022-04-07 16:17:23 +0100

    cart dropdown

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -8,7 +8,7 @@ import CartIcon from '../cart-icon/cart-icon';
 import './Header.scss'
 import CartDropdown from '../cart-dropdown/cart-dropdown';
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
     <div className='header'>
       <Link className='logo-container' to='/'>
         <Logo className='logo' />
@@ -31,12 +31,15 @@ const Header = ({ currentUser }) => (
         )}
         <CartIcon/>
       </div>
-      <CartDropdown/>
+      {
+          hidden ? null : <CartDropdown/>
+      }
     </div>
   );
   
-  const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
+  const mapStateToProps = ({user: {currentUser}, cart: {hidden}}) => ({
+    currentUser,
+    hidden
   });
   
   export default connect(mapStateToProps)(Header);
--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,10 +1,11 @@
+import CartActionTypes from "./cart.types";
 const INITIAL_STATE = {
     hidden: true
 };
 
 const cartReducer = (state = INITIAL_STATE, action) => {
     switch (action.type) {
-        case 'TOGGLE_CART_HIDDEN':
+        case CartActionTypes.TOGGLE_CART_HIDDEN:
             return {
                 ...state,
                 hidden: !state.hidden
@@ -13,3 +14,5 @@ const cartReducer = (state = INITIAL_STATE, action) => {
             return state;
     }
 }
+
+export default cartReducer
